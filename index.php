48899305f2465b64f17f34e9c23a90fe97ac0461
LouisCrl <louis.carlu@viacesi.fr>
2025-03-31 16:56:10 +0200

    commit

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require_once "src/Controllers/CompanyController.php";
 require_once "src/Controllers/RatingController.php";
 require_once "src/Controllers/OfferController.php";
 require_once "src/Controllers/UserController.php";
-require_once "src/Controllers/ApplicationController.php"; // Bon chemin
+require_once "src/Controllers/ApplicationController.php";
 
 use App\Controllers\CompanyController;
 use App\Controllers\RatingController;
@@ -20,28 +20,23 @@ $twig = new \Twig\Environment($loader, [
 $CompanyController = new CompanyController($twig);
 $OfferController = new OfferController($twig);
 $UserController = new UserController($twig);
-<<<<<<< HEAD
-$ApplicationController = new ApplicationController($twig);
-=======
 $RatingController = new RatingController();
->>>>>>> c13d5b3afeb9da6091e2222859095aa0d3552f3e
+$ApplicationController = new ApplicationController($twig);
 
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
-<<<<<<< HEAD
-=======
 
 $id_role = $_SESSION['id_role'] ?? null;
 $firstname = $_SESSION['firstname'] ?? null;
 $idUser = $_SESSION['idUser'] ?? null;
->>>>>>> c13d5b3afeb9da6091e2222859095aa0d3552f3e
 
 $uri = '/';
 
 $keywords = null;
 $duration = null;
 $experience = null;
+
 
 if (isset($_GET['action']) && $_GET['action'] !== '') {
     if ($_GET['action'] === 'authenticate') {
@@ -52,19 +47,16 @@ if (isset($_GET['action']) && $_GET['action'] !== '') {
     if ($_GET['action'] == 'disconnect') {
         $UserController->disconnect();
     }
-<<<<<<< HEAD
     if ($_GET['action']=='filteroffers'){
         $keywords = $_POST['keywords'] ?? null;
         $duration = $_POST['duration'] ?? null;
         $experience = $_POST['experience'] ?? null;
 
         $uri='/browse';
-    
-=======
+    }
     if($_GET['action']=='rateCompany'){
         $RatingController->addNote(intval($_POST['idCompany']), intval($idUser), intval($_POST['rate']));
         header("Location: ?uri=/details-company&id=".$_POST['idCompany']);
->>>>>>> c13d5b3afeb9da6091e2222859095aa0d3552f3e
     }
 }
 
@@ -72,21 +64,13 @@ elseif (isset($_GET['uri'])) {
     $uri = $_GET['uri'];
 }
 
-<<<<<<< HEAD
-$id_role = $_SESSION['id_role'] ?? null;
-$firstname = $_SESSION['firstname'] ?? null;
-
-=======
->>>>>>> c13d5b3afeb9da6091e2222859095aa0d3552f3e
 switch ($uri) {
     case '/':
-        $OfferController->printOffers('home.twig', 3,[]);
+        $OfferController->printOffers('home.twig', 3, []);
         break;
-
     case '/browse':
-        $OfferController->printOffers('browse.twig', 5,[$keywords,$duration,$experience]);
+        $OfferController->printOffers('browse.twig', 5, [$keywords,$duration,$experience]);
         break;
-
     case '/details-offer':
         $offerId = $_GET['id'] ?? null;
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
@@ -99,51 +83,30 @@ switch ($uri) {
         // Affichage de l'offre spécifique
         $OfferController->printSpecificOffer($offerId);
         break;
-
     case '/create-offer':
         echo $twig->render('create-offer.twig', ['firstname' => $firstname, 'id_role' => $id_role]);
         break;
-
     case '/support':
         echo $twig->render('support.twig', ['firstname' => $firstname, 'id_role' => $id_role]);
         break;
-
     case '/connection':
-<<<<<<< HEAD
-        if (isset($firstname)) {
-            echo $twig->render('profile.twig', ['firstname' => $firstname, 'id_role' => $id_role]);
-        } else {
-            echo $twig->render('connection.twig', ['firstname' => $firstname, 'id_role' => $id_role]);
-=======
         if(isset($firstname)){
             echo $twig->render('profile.twig',['firstname' => $firstname,'id_role' =>$id_role, 'isUser' => true]);
         }
         else{
             echo $twig->render('connection.twig',['firstname' => $firstname,'id_role' =>$id_role]);
->>>>>>> c13d5b3afeb9da6091e2222859095aa0d3552f3e
         }
         break;
-
     case '/connectionwrong':
         echo $twig->render('connection.twig', ['firstname' => $firstname, 'id_role' => $id_role, 'connected' => "false"]);
         break;
-
     case '/sign-up':
         echo $twig->render('sign-up.twig', ['firstname' => $firstname, 'id_role' => $id_role]);
         break;
-<<<<<<< HEAD
-
-    case '/details-entreprise':
-=======
-    case '/note':
-        echo $twig->render('note.twig',['firstname' => $firstname,'id_role' =>$id_role]);
-        break;
     case '/details-company':
->>>>>>> c13d5b3afeb9da6091e2222859095aa0d3552f3e
         $companyId = $_GET['id'];
         $CompanyController->printCompany($companyId, $firstname, $id_role);
         break;
-
     default:
         echo 'Page not found';
         break;

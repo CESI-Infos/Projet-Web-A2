605cf68c63c4c4d89cae7672618d775a79166c32
maelpitois <mael.pitois@viacesi.fr>
2025-03-31 11:06:05 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,79 @@
+<?php
+require "vendor/autoload.php";
+require_once "src/Controllers/CompanyController.php";
+require_once "src/Controllers/OfferController.php";
+require_once "src/Controllers/UserController.php";
+
+use App\Controllers\CompanyController;
+use App\Controllers\OfferController;
+use App\Controllers\UserController;
+
+$loader = new \Twig\Loader\FilesystemLoader('templates');
+$twig = new \Twig\Environment($loader, [
+    'debug' => true
+]);
+
+$CompanyController = new CompanyController($twig);
+$OfferController = new OfferController($twig);
+$UserController = new UserController($twig);
+
+$uri = '/';
+
+if (isset($_GET['action']) && $_GET['action'] !== '') {
+    if($_GET['action']==='authenticate'){
+        $mail=$_POST["mail"];
+        $password=$_POST["password"];
+        $UserController->authenticate($mail,$password);
+
+    }
+    if($_GET['action']=='disconnect'){
+        $UserController->disconnect();
+    }
+}
+
+elseif (isset($_GET['uri'])) {
+    $uri = $_GET['uri'];
+}
+
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+$id_role = $_SESSION['id_role'] ?? null;
+$firstname = $_SESSION['firstname'] ?? null;
+
+switch ($uri) {
+    case '/':
+        $OfferController->printOffers('home.twig', 3);
+        break;
+    case '/browse':
+        $OfferController->printOffers('browse.twig', 5);
+        break;
+    case '/details-offer':
+        $offerId = $_GET['id'];
+        $OfferController->printSpecificOffer($offerId);
+        break;
+    case '/create-offer':
+        echo $twig->render('create-offer.twig',['firstname' => $firstname,'id_role' =>$id_role]);
+        break;
+    case '/support':
+        echo $twig->render('support.twig',['firstname' => $firstname,'id_role' =>$id_role]);
+        break;
+    case '/connection':
+        if(isset($firstname)){
+            echo $twig->render('profile.twig',['firstname' => $firstname,'id_role' =>$id_role]);
+        }
+        else{
+            echo $twig->render('connection.twig',['firstname' => $firstname,'id_role' =>$id_role]);
+        }
+        break;
+    case '/connectionwrong':
+        echo $twig->render('connection.twig',['firstname' => $firstname,'id_role' =>$id_role,'connected'=>"false"]);
+        break;
+    case '/sign-up':
+        echo $twig->render('sign-up.twig',['firstname' => $firstname,'id_role' =>$id_role]);
+        break;
+    default:
+        echo 'Page not found';
+        break;
+}
